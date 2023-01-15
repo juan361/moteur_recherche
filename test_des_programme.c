54649becf2835b2dd77c28dd2256cc8b96056c79
Mathias Audibert <mathias.audibert@isen.yncrea.fr>
2023-01-15 12:09:40 +0100

    commit de quelques modification

--- test_des_programme.c
+++ test_des_programme.c
@@ -4,8 +4,10 @@
 #include <stdlib.h>
 
 
-//#define PATH "/home/isen/Algo/MotdeRech/moteur_recherche/textes"
-char** filename(char* PATH,char FileName[3][20])
+
+#define TXT_PATH "/home/isen/Algo/MotdeRech/moteur_recherche/textes_TXT/"
+#define CRI_PATH "/home/isen/Algo/MotdeRech/moteur_recherche/textes_CRI/"
+char** filename(char* PATH,char *FileName[3][20])
 {
 	DIR* dir=opendir(PATH);
 	int i=0;
@@ -24,7 +26,6 @@ char** filename(char* PATH,char FileName[3][20])
 		{
 			//FileName[i] = (char*)malloc(strlen(entity->d_name) + 1);
 			c=entity->d_name;
-			strncpy(FileName[i],c , strlen(c) - 4);
 			i++;
         	}
 		entity= readdir(dir);
@@ -33,17 +34,54 @@ char** filename(char* PATH,char FileName[3][20])
         	{free(FileName[i]);}
         free(FileName);*/
 	closedir(dir);
-	return(FileName);
+}
+void Crea_CIR(char TxtName)
+{
+	char *docPath = malloc(strlen(TXT_PATH) + strlen(TxtName) + 1);
+	docPath = strcpy(docPath, TXT_PATH);
+	docPath = strcat(docPath, TxtName); 
+	FILE *doc = fopen(docPath, "r");
+	
+	char *CIRPath = malloc(strlen(CRI_PATH) + strlen(TxtName) + 1);
+	if(strstr(TxtName, ".txt") == NULL)
+	{
+		return;
+	}
+	//crée une version de la variable TxtName sans extension
+	char *SimpleName = malloc(strlen(TxtName) - 4 + 1);
+	strncpy(SimpleName, TxtName, strlen(TxtName) - 4);
+	SimpleName[strlen(TxtName) - 4] = '\0';
+	
+	strcpy(CIRPath, CRI_PATH);
+	strcat(CIRPath, SimpleName);
+	strcat(CIRPath, ".CRI");
+	FILE *index = fopen(CIRPath, "w");
+	
+	//vérifie si le fichier txt c'est bien ouvert
+	if (doc == NULL) 
+	{
+		printf("Error opening document : %s\n", docPath);
+		exit(1);
+	}
+	// vérifie si le fichier CRI c'est bien ouvert
+	if (index == NULL) 
+	{
+		printf("Error opening/creating index file %s\n", CIRPath);
+		exit(1);
+	}
 }
 
 int main(int argc, char* argv[]) 
 {
 	//char **FileName = (char**)malloc(sizeof(char *) * 3);
 	char test[3][20];
-    	filename("/home/isen/Algo/MotdeRech/moteur_recherche/textes",test);
+    	filename("/home/isen/Algo/MotdeRech/moteur_recherche/textes_txt",test);
     for (int i=0;i<3;i++)
     {
     	printf("%s\n",test[i]);
+    	puts("c");
     }
+    Crea_CIR("brain.txt");
     return 0;
 }
+
