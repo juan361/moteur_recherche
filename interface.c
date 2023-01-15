bf6f2050a64f37d0e96a137f36d1a1788572da22
atlas <jgsan360@gmail.com>
2023-01-15 12:04:45 +0100

    modification rapide de gitignore

--- interface.c
+++ interface.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
+
+
 int main(){
-    char string_scan[250], string_maj[250];
+    char string_scan[250], string_maj[250], string_min[250];
 
     printf("__moteur de recherche__ \n");
     printf("What do you want to search ? : \n");
@@ -13,8 +16,9 @@ int main(){
 
     for (int i = 0; i < strlen(string_scan); i++) {
       string_maj[i] = toupper(string_scan[i]);
+      string_min[i] = tolower(string_scan[i]);
   }
 
-    printf("vous recherchez %s \nnous rechercherons alors avec le mot %s \n", string_scan, string_maj);
+    printf("vous recherchez %s \nnous rechercherons alors avec le mot %s \n, ou %s", string_scan, string_maj, string_min);
     return 0;
 }
