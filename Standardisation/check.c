8fe63ec45a0c861126ae7f1c7675bdad108a6de7
atlas <jgsan360@gmail.com>
2023-01-15 14:17:30 +0100

    jcréation de la fonction wEqual permettant de comparer deux mots

---
+++ Standardisation/check.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <errno.h>
+
+#define DOC_PATH "./docs/"
+#define INDEX_PATH "./index/"
+
+
+__MINGW_EXTENSION typedef unsigned __int64 size_t;
+typedef enum { false, true } bool;
+
+char *toLowerCaseAndRemoveSymbols(const char *word){
+    size_t len = strlen(word);
+    char *lowerCaseWord = (char *) malloc(len + 1);
+
+    for (size_t i = 0; i < len; i++)
+    {
+        if(word[i] >= 'A' && word[i] <= 'Z'){
+            lowerCaseWord[i] = word[i] + 32;
+        } else {
+            lowerCaseWord[i] = word[i];
+        }
+    }
+    lowerCaseWord[len] = '\0';
+
+    for(size_t i = 0; i < len; i++){
+        if (lowerCaseWord[i] == '.' || lowerCaseWord[i] == ',' || lowerCaseWord[i] == ';' || lowerCaseWord[i] == ':' || lowerCaseWord[i] == '!' || lowerCaseWord[i] == '?' || lowerCaseWord[i] == '"' || lowerCaseWord[i] == '(' || lowerCaseWord[i] == ')' || lowerCaseWord[i] == '\'' || lowerCaseWord[i] == '\n'){
+            lowerCaseWord[i] = '\0';
+            break;
+        }
+    }
+
+    return lowerCaseWord;
+}
+
+char *englishSing(const char *word) {
+    size_t len = strlen(word);
+    char *singular = (char *) malloc(len + 1);
+    if (len > 2 && word[len - 1] == 's') {
+        if(
+            (word[len - 2] == 'e' && word[len - 3] == 'i')
+            || (word[len - 2] == 'a')
+        ){
+            // word is already in singular form
+            strcpy(singular, word);
+        } else {
+            strncpy(singular, word, len - 1);
+            singular[len - 1] = '\0';
+        }
+    } else {
+        //word is already in singular form
+        strcpy(singular, word);
+    }
+
+    return singular;
+}
+
+
+bool wEqual(const char *word1, const char *word2){
+    char *word1C = malloc(strlen(word1) + 1);
+    char *word2C = malloc(strlen(word2) + 1);
+    strcpy(word1C, word1);
+    strcpy(word2C, word2);
+    char *word1L = toLowerCaseAndRemoveSymbols(word1C);
+    char *word2L = toLowerCaseAndRemoveSymbols(word2C);
+    char *word1S = englishSing(word1C);
+    char *word2S = englishSing(word2C);
+    bool equal = strcmp(word1S, word2S) == 0;
+    free(word1C);
+    free(word2C);
+    free(word1L);
+    free(word2L);
+    free(word1S);
+    free(word2S);
+    return equal;
+}
