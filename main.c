90e461aa5f8e9cbd393cb3914e49a537667dd733
atlas <jgsan360@gmail.com>
2023-01-19 00:22:02 +0100

    l'interface marche

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <unistd.h>
 
 #define INDEX_PATH "./textes_CRI/"
 
@@ -12,43 +13,55 @@ void searchWords(const char *word);
 
 
 int main(){
-    char string_scan[250];
-    puts(" ----------------------");
-    puts("|  ▄▄▄·  ▄▄· • ▌ ▄ ·.  |");
-    puts("| ▐█ ▀█ ▐█ ▌▪·██ ▐███▪ |");
-    puts("| ▄█▀▀█ ██ ▄▄▐█ ▌▐▌▐█· |");
-    puts("| ▐█ ▪▐▌▐███▌██ ██▌▐█▌ |");
-    puts("|  ▀  ▀ ·▀▀▀ ▀▀  █▪▀▀▀ |");
-    puts("|______________________|");
-     
-          _____                    _____                    _____          
-         /\    \                  /\    \                  /\    \         
-        /::\    \                /::\    \                /::\____\        
-       /::::\    \              /::::\    \              /::::|   |        
-      /::::::\    \            /::::::\    \            /:::::|   |        
-     /:::/\:::\    \          /:::/\:::\    \          /::::::|   |        
-    /:::/__\:::\    \        /:::/  \:::\    \        /:::/|::|   |        
-   /::::\   \:::\    \      /:::/    \:::\    \      /:::/ |::|   |        
-  /::::::\   \:::\    \    /:::/    / \:::\    \    /:::/  |::|___|______  
- /:::/\:::\   \:::\    \  /:::/    /   \:::\    \  /:::/   |::::::::\    \ 
-/:::/  \:::\   \:::\____\/:::/____/     \:::\____\/:::/    |:::::::::\____\
-\::/    \:::\  /:::/    /\:::\    \      \::/    /\::/    / ~~~~~/:::/    /
- \/____/ \:::\/:::/    /  \:::\    \      \/____/  \/____/      /:::/    / 
-          \::::::/    /    \:::\    \                          /:::/    /  
-           \::::/    /      \:::\    \                        /:::/    /   
-           /:::/    /        \:::\    \                      /:::/    /    
-          /:::/    /          \:::\    \                    /:::/    /     
-         /:::/    /            \:::\    \                  /:::/    /      
-        /:::/    /              \:::\____\                /:::/    /       
-        \::/    /                \::/    /                \::/    /        
-         \/____/                  \/____/                  \/____/         
-                                                                           
-                                                                           
+    
+    char string_scan[250], string_min[250];
+    int x=1, i=10, c;
 
+    while (x != 0)
+    {
+        puts(" .----------------.  .----------------.  .----------------. ");
+        puts("| .--------------. || .--------------. || .--------------. |");
+        puts("| | ____    ____ | || |      __      | || |     ______   | |");
+        puts("| ||_   \\  /   _|| || |     /  \\     | || |   .' ___  |  | |");
+        puts("| |  |   \\/   |  | || |    / /\\ \\    | || |  / .'   \\_|  | |");
+        puts("| |  | |\\  /| |  | || |   / ____ \\   | || |  | |         | |");
+        puts("| | _| |_\\/_| |_ | || | _/ /    \\ \\_ | || |  \\ `.___.'\\  | |");
+        puts("| ||_____||_____|| || ||____|  |____|| || |   `._____.'  | |");
+        puts("| |              | || |              | || |              | |");
+        puts("| '--------------' || '--------------' || '--------------' |");
+        puts(" '----------------'  '----------------'  '----------------' ");
 
+        puts("What do you want to do ?");
+        puts("0. Quit");
+        puts("1. Do a research ");
+        scanf("%i", &c);
+        switch (c)
+        {
+            case 1:
+            puts("What do you want to search ? : \n");
+            scanf("%s", &string_scan);
 
-    printf("What do you want to search ? : \n");
+            for (int i = 0; i < strlen(string_scan); i++) {
+                string_min[i] = tolower(string_scan[i]);
+            }
+            /*
+            for(int i = 1; i < argc; i++) {
+                searchDocuments(argv[i]);
 
-    scanf("%s", &string_scan);
-    return 0;
+            }
+            */
+            printf("Vous recherchez %s \nnous rechercherons alors avec le mot %s \n", string_scan, string_min);
+            //TODO on mettra le petit programme pour chercher le programme
+            break;
+            case 0:
+            x = 0;
+            break;
+        default:
+            puts("Please choose between 0 and 1, quit or do some research");
+            x = 1;
+
+            break;
+        }
+    }
+    return 1;
 }
